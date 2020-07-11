16e9a4948ed0c426c665ccd5cb745ff7fe3f8d1e
bouzuya <m@bouzuya.net>
2020-07-11 23:01:38 +0900

    tsc --init & add sample files

---
+++ src/index.test.ts
@@ -0,0 +1,4 @@
+import assert from "assert";
+import { parse } from "./index";
+
+assert.deepStrictEqual(parse(""), []);
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export function parse(_: string): string[][] {
+  return [];
+}
